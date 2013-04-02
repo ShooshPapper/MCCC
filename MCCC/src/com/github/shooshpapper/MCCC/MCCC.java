fc9283bc45d1065d961d44eab2d8400406ad79e6
Ken Rushia <ken@kenrushia.com>
2013-04-02 14:53:30 -0400

    Cleaned out comments, config, and tested for release

--- MCCC/src/com/github/shooshpapper/MCCC/MCCC.java
+++ MCCC/src/com/github/shooshpapper/MCCC/MCCC.java
@@ -6,10 +6,12 @@ package com.github.shooshpapper.MCCC;
 
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.SignChangeEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
-import java.text.Normalizer;
+//import java.text.Normalizer;
 
 
 
@@ -23,69 +25,58 @@ public class MCCC extends JavaPlugin implements Listener {
 	
     @Override
     public void onEnable() {
-    	plugin.getConfig().options().copyDefaults(true);
+    	saveDefaultConfig();
     	getServer().getPluginManager().registerEvents(this, this);
     }
 	
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onPlayerChat(AsyncPlayerChatEvent event) {
+		if (event.isCancelled()) {
+			return;
+		}
 		String message = event.getMessage();
-
-		// TODO: remove format codes
-		//        §1f§2u§3c§4k --> fuck
-		
-		// TODO: if message contains non-alphanumerics, translate them
-		//        ƒµ©K --> fuck
-		// We can't normalize without going the whole way, because NFD can alter character count
-		// String newMessage = Normalizer.normalize(message, Normalizer.Form.NFD);
-		
-		// TODO: multi-character glyphs
-		//        fuc|< --> fuck
-		
-		// TODO: case mapping
-		//        FUcK --> fuck
+		event.setMessage(censor(message));
+	}
+	
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void onPlayerCommand(PlayerCommandPreprocessEvent event) {
+		if (event.isCancelled()) {
+			return;
+		}
+		String message = event.getMessage();
+		if (message.startsWith("/me")) {
+			event.setMessage(censor(message));
+		}
+		else if (message.startsWith("/tell")) {
+			// TODO: Split first so target username isn't censored
+			event.setMessage(censor(message));
+		}
+		else if (message.startsWith("/say")) {
+			event.setMessage(censor(message));
+		}
+	}
+	
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void onSignChange(SignChangeEvent event) {
+		if (event.isCancelled()) {
+			return;
+		}
+		int i = 0;
+		for (String line : event.getLines()) {
+			event.setLine(i, censor(line));
+			i++;
+		}
+	}
+	
+	private String censor(String message) {	
+		// case mapping
+		//        BAnanA --> banana
 		String testMessage = message.toLowerCase();
-		
-		// TODO: space/separator removal
-		//        f u c k --> fuck
-		//        f.u.c.k --> fuck
-		//        f█u█c█k --> fuck
-		
-		// TODO: non-dictionary word with alphabetic spacing
-		//        aaFaaaUaaCaaKaa --> fuck
-		
-		// TODO: intact first and last character
-		//        fcuk --> fuck
-		
-		// NOTE: This is actually a lie (somewhat) and called typoglycemia
-		//        https://en.wikipedia.org/wiki/Typoglycemia
-		//        http://www.mrc-cbu.cam.ac.uk/people/matt.davis/cmabridge/
 
-
-		// TODO: pre/postfix removal
-		//        assmonster --> ass
-		//        unclefucker --> fuck
-		//        fatniggerbumhole --> ?
-		
-		// TODO: multiline concatenation
-		//        f
-		//        u
-		//        c
-		//        k --> fuck
-		
-		// TODO: repeated character removal
-		//        fffuuuuuuuucccckkk --> fuck
-		
-		// TODO: phonetic matching
-		//        fuk --> fuck
-		
-		// TODO: grab fucks from a list/db
-		
-		// TODO: stemming
-		//        fucking --> fuck
-		
-		for (String swear : plugin.getConfig().getStringList("swear")) {
+		Boolean doReplace = false;
+		for (String swear : this.getConfig().getStringList("swears")) {
 			if (testMessage.contains(swear)) {
+				doReplace = true;
 				StringBuilder replacement = new StringBuilder();
 				for (int i = 0; i < swear.length(); i++) {
 					replacement.append("*");
@@ -93,12 +84,17 @@ public class MCCC extends JavaPlugin implements Listener {
 				testMessage = testMessage.replaceAll(swear, replacement.toString());
 			}
 		}
-		StringBuilder newMessage = new StringBuilder(message);
-		for (int i = 0; i < message.length(); i++) {
-			if (testMessage.charAt(i) == '*') {
-				newMessage.setCharAt(i, '*');
+		if (doReplace) {
+			StringBuilder newMessage = new StringBuilder(message);
+			for (int i = 0; i < message.length(); i++) {
+				if (testMessage.charAt(i) == '*') {
+					newMessage.setCharAt(i, '*');
+				}
 			}
+			return newMessage.toString();
 		}
-		event.setMessage(newMessage.toString());
+		else {
+			return message;
+		}
 	}
 }
